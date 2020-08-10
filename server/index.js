5310ca7c6746fbb0ca433368bef87d63581b0aec
unknown <leonard9619@gmail.com>
2020-08-10 21:41:22 +0200

    refactored into routes and services

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const app = express();
+require ('./services/passport');
 
-app.get('/', (req, res) => {
-    res.send({hi: 'hello'});
-});
+
+
+const app = express();
+require ('./routes/authRoutes')(app);
+
+
+
+
 
 
 const PORT = process.env.PORT || 5000;
