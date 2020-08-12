73ab2041f459bc0f5fbc6c718cca99608af6de71
Leonard <leonard.durmishi@akshi.gov.al>
2020-08-12 11:59:17 +0200

    div prod from dev

---
+++ Config/keys.js
@@ -0,0 +1,9 @@
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
+
+prod
+//632850589065-2icha9sk6h49qdo3vispdoane1ss2p6d.apps.googleusercontent.com
+//EJl1ftzqXYkB4QPEL-6ojWZ2
---
+++ Config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+//632850589065-2icha9sk6h49qdo3vispdoane1ss2p6d.apps.googleusercontent.com
+//EJl1ftzqXYkB4QPEL-6ojWZ2
